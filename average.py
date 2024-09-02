cb16cf6c916d936678c844ca9d5df62029858418
HP@LAPTOP-8SD00Q9L <aranjan946@gmail.com>
2024-09-03 01:02:52 +0530

    average of three number

---
+++ average.py
@@ -0,0 +1,9 @@
+number1 = float(input("Enter the first number: "))  # Prompt for the first number
+number2 = float(input("Enter the second number: ")) # Prompt for the second number
+number3 = float(input("Enter the third number: "))   # Prompt for the third number
+
+    # Calculate the average
+average = (number1+ number2+ number3)/3
+
+    # Display the result using % formatting
+print("The average of the three numbers is: %.2f" % average)
