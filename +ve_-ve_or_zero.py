ec0f2032931cb97f5011b1f5ed45849e37def0f2
HP@LAPTOP-8SD00Q9L <aranjan946@gmail.com>
2024-09-03 00:59:55 +0530

    positive negative or zero

---
+++ +ve_-ve_or_zero.py
@@ -0,0 +1,10 @@
+# Function to determine if the number is positive, negative, or zero
+
+number=int(input("enter the number"))
+
+if number > 0:
+    print("positive")
+elif number < 0:
+    print("negative")
+else:
+    print("zero")
