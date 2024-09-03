70f81379b47d75ff008b03e886d244bb6e9dc602
HP@LAPTOP-8SD00Q9L <aranjan946@gmail.com>
2024-09-03 12:55:00 +0530

    reverse the name

---
+++ reverse_name.py
@@ -0,0 +1,10 @@
+# Get the user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Reverse the order of the names
+reversed_name = last_name + " " + first_name
+
+# Print the reversed name
+print("Your name in reverse order:", reversed_name)
+
