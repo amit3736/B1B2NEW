d85c9ca82f2f3620943911f6bf45fcc22f9c36aa
HP@LAPTOP-8SD00Q9L <aranjan946@gmail.com>
2024-09-03 12:52:32 +0530

    using the string method

---
+++ method_string_using.py
@@ -0,0 +1,17 @@
+# upper() function to convert
+# string to upper case
+text = 'HellO Upes DehradUn'
+
+print("\nConverted String to upper case:")
+print(text.upper())
+
+# lower() function to convert
+# string to lower case
+print("\nConverted String to lower case:")
+print(text.lower())
+
+
+
+#String methods used are
+#lower():Converts all uppercase characters in a string into lowercase
+#Upper(): Converts all lowercase characters in a string into uppercase
