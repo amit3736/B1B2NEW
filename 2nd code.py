f26d5da62864054191e14b333f3b6f41d900f25e
Amit Ranjan <aranjan946@gmail.com>
2024-10-08 09:44:52 +0530

    distance and point

---
+++ 2nd code.py
@@ -0,0 +1,24 @@
+#(0,1),(2,4) , (3,6) , (7,7)
+
+class Point:
+    def__init__(self,x,y):
+        self.x=x
+        self.y=y
+
+   def distance(self.otherpoint):
+       x1*self.x
+       y1*self.y
+       x2*otherpoint.x
+       y2*otherpoint.y
+
+       d=( (x2-x1)*2+(y2-Y1)*2)**(1/2)
+       return d
+
+p1=point(0,1) #intiating object
+p2=point(2,4)
+p3=point(3.6)
+p4=point(7,7)
+
+print(p1)
+print(p1.x)
+print(p1.y)
